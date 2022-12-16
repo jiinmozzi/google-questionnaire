ba2fc8724f3a36a355f58e14fea61c53ce961477
jho378 <jho378@naver.com>
2022-12-16 19:30:56 +0900

    [feature] connected router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import React from 'react';
+import Home from "./pages/Home";
+import Preview from "./pages/Preview";
+import Response from "./pages/Response";
+
+import "./styles/reset.scss";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/preview" element={<Preview />}></Route>
+          <Route path="/repsonse" element={<Response />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
