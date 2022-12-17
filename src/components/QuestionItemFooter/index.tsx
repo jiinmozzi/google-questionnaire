0e00b4a00be0fa28e4f5d84f1e957d6df292e35f
jho378 <jho378@naver.com>
2022-12-17 17:05:44 +0900

    [feat] 개별 질문 및 설명의 푸터부분 레이아웃

--- src/components/QuestionItemFooter/index.tsx
+++ src/components/QuestionItemFooter/index.tsx
@@ -1,8 +1,34 @@
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import DeleteSweepRoundedIcon from '@mui/icons-material/DeleteSweepRounded';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
 import "./QuestionItemFooter.scss";
+import React from 'react';
 
 const QuestionItemFooter = () => {
+    const onChange = (e : React.SyntheticEvent) => {
+        console.log(e);
+    }
     return (
-        <div className="question-item-footer"></div>
+        <div className="question-item-footer">
+            <div id="question-control-icon-wrapper">
+                <div className="footer-icon-wrapper">
+                    <ContentCopyIcon className="footer-icon"/>
+                </div>
+                <div className="footer-icon-wrapper">
+                    <DeleteSweepRoundedIcon className="footer-icon"/>
+                </div>
+            </div>
+            <FormControlLabel 
+                control={<Switch />} 
+                onChange={onChange}
+                label="필수"
+                labelPlacement='start' 
+            />
+            <MoreVertIcon id="footer-option-icon" className="footer-icon"/>
+        </div>
     )
 }
 
