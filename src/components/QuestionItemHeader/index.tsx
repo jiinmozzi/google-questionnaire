b08e6610f1f53f33458c46992067ede8730d873d
jho378 <jho378@naver.com>
2022-12-18 02:54:49 +0900

    [feature] 다양한 형식의 질문 제작 중 겹치는 헤더 부분 컴포넌트화 완료

--- src/components/QuestionItemHeader/index.tsx
+++ src/components/QuestionItemHeader/index.tsx
@@ -1,8 +1,24 @@
+import QuestionTypeDropDown from "../QuestionTypeDropDown";
+import InsertPhotoOutlinedIcon from '@mui/icons-material/InsertPhotoOutlined';
 import "./QuestionItemHeader.scss";
+import { useState } from "react";
 
 const QuestionItemHeader = () => {
+    const [focused, setFocused] = useState<boolean>(false); 
     return (
-        <div className="question-item-header-wrapper"></div>
+        <div className="question-item-header-wrapper">
+            <div className="question-title-wrapper">
+                <input 
+                    type="text" 
+                    className="question-title-input" 
+                    placeholder="질문" 
+                    onFocus={() => setFocused(true)} 
+                    onBlur={() => setFocused(false)}/>
+                    
+            </div>
+            <InsertPhotoOutlinedIcon className="photo-disabled"/>
+            <QuestionTypeDropDown />
+        </div>
     )
 }
 
