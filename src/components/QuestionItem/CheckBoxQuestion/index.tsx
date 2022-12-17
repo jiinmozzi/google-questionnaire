3c6177825f9af56cbbb1de3b0a59696aeff9df4a
jho378 <jho378@naver.com>
2022-12-17 23:37:43 +0900

    [feat] 체크박스 제작 시 퍼블리싱 완료

--- src/components/QuestionItem/CheckBoxQuestion/index.tsx
+++ src/components/QuestionItem/CheckBoxQuestion/index.tsx
@@ -1,9 +1,38 @@
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import CheckBoxOutlineBlankRoundedIcon from '@mui/icons-material/CheckBoxOutlineBlankRounded';
+
 import "./CheckBoxQuestion.scss"
+
+type CheckBoxQuestionChoicePropsType = {
+    idx : number
+}
+
+const CheckBoxQuestionChoice = ({ idx } : CheckBoxQuestionChoicePropsType) => {
+    return (
+        <div className="checkbox-question-choice-wrapper">
+            <label htmlFor={`choice-${idx}`} className="checkbox-question-label">
+                <CheckBoxOutlineBlankRoundedIcon className="checkbox-question-mark"/>    
+            </label>
+            {/* <RadioButtonUncheckedIcon className="checkbox-question-mark"/> */}
+            <input className="checkbox-question-input" type="text" id={`choice-${idx}`} defaultValue={`옵션 ${idx}`}/>
+            <CloseRoundedIcon className="choice-delete-icon"/>
+        </div>
+    )
+}
 
 const CheckBoxQuestion = () => {
     return (
         <div className="checkbox-question-wrapper">
-            checkbox
+            <CheckBoxQuestionChoice idx={1}/>
+            <CheckBoxQuestionChoice idx={2}/>
+            <div className="checkbox-add-indicator">
+                <CheckBoxOutlineBlankRoundedIcon className="choice-add-icon"/>    
+                <div className="choice-add-div">
+                    <span id="add-option">옵션 추가</span>&nbsp;
+                    <span>또는</span>&nbsp;
+                    <span id="add-others">'기타' 추가</span>
+                </div>
+            </div>
         </div>
     )
 }
