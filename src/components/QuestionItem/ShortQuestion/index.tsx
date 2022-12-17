a12052efc2c5c831528112dca0269cf14cd505d6
jho378 <jho378@naver.com>
2022-12-17 20:34:12 +0900

    [feat] 단답형 질문 제작 시 Layout 완료

--- src/components/QuestionItem/ShortQuestion/index.tsx
+++ src/components/QuestionItem/ShortQuestion/index.tsx
@@ -3,7 +3,7 @@ import "./ShortQuestion.scss";
 const ShortQuestion = () => {
     return (
         <div className="short-question-wrapper">
-            short
+            <input type="text" className="short-question-input" placeholder="단답형 텍스트"/>
         </div>
     )
 }
