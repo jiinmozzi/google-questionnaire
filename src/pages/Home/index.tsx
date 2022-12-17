cd828ad90ba7dfff3adb594386bcc47edaf8b60c
jho378 <jho378@naver.com>
2022-12-18 02:55:10 +0900

    [feature] 홈 화면 랜더링 완성

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,15 @@
+import "./Home.scss";
+import Header from "../../components/Header";
+import QuestionItemList from "../../components/QuestionItemList";
+import SideBar from "../../components/SideBar";
+import { useRef } from "react";
+
 const Home = () => {
     return (
         <div id="home-wrapper">
-            
+            <Header />
+            <QuestionItemList />
+            <SideBar />
         </div>
     ) 
 }
