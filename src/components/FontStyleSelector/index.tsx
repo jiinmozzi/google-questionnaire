9b1f4d7938eff0c3b37b34a5023d83bc90e90644
jho378 <jho378@naver.com>
2022-12-18 02:55:58 +0900

    [fix] 글씨 폰트 조절하는 컴포넌트 JSX 구조 수정

--- src/components/FontStyleSelector/index.tsx
+++ src/components/FontStyleSelector/index.tsx
@@ -9,12 +9,23 @@ import React from 'react';
 
 const FontStyleSelector = () => {
     return (
-        <div className="font-style-selector-wrapper">
-            <FormatBoldIcon className="font-style-select-icon" onMouseDown={(e : React.MouseEvent) => e.preventDefault()}/>
-            <FormatUnderlinedIcon className="font-style-select-icon" onMouseDown={(e : React.MouseEvent) => e.preventDefault()}/>
-            <FormatItalicIcon className="font-style-select-icon" onMouseDown={(e : React.MouseEvent) => e.preventDefault()}/>
-            <InsertLinkIcon className="font-style-select-icon" onMouseDown={(e : React.MouseEvent) => e.preventDefault()} />
-            <FormatClearIcon className="font-style-select-icon" onMouseDown={(e : React.MouseEvent) => e.preventDefault()}/>
+        <div className="font-style-selector-wrapper" onMouseDown={(e : React.MouseEvent) => e.preventDefault()}>
+            <div className="font-icon-wrapper">
+                <FormatBoldIcon className="font-style-select-icon" />
+            </div>
+            <div className="font-icon-wrapper">
+                <FormatUnderlinedIcon className="font-style-select-icon" onMouseDown={(e : React.MouseEvent) => e.preventDefault()}/>
+            </div>
+            <div className="font-icon-wrapper">
+                <FormatItalicIcon className="font-style-select-icon" onMouseDown={(e : React.MouseEvent) => e.preventDefault()}/>
+            </div>
+            <div className="font-icon-wrapper">
+                <InsertLinkIcon className="font-style-select-icon" onMouseDown={(e : React.MouseEvent) => e.preventDefault()} />
+            </div>
+            <div className="font-icon-wrapper">
+                <FormatClearIcon className="font-style-select-icon" onMouseDown={(e : React.MouseEvent) => e.preventDefault()}/>
+            </div>
+            
         </div>
     )
 }
