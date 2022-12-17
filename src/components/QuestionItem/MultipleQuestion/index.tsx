c924f849378ce45bc59635a9a1f0f23f95c74254
jho378 <jho378@naver.com>
2022-12-17 23:36:57 +0900

    [feat] 객관식 제작 시 퍼블리싱 완료

--- src/components/QuestionItem/MultipleQuestion/index.tsx
+++ src/components/QuestionItem/MultipleQuestion/index.tsx
@@ -1,9 +1,38 @@
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+
 import "./MultipleQuestion.scss";
+
+type MultipleQuestionChoicePropsType = {
+    idx : number
+}
+
+const MultipleQuestionChoice = ( {idx} : MultipleQuestionChoicePropsType ) => {
+    return (
+        <div className="multiple-question-choice-wrapper">
+            <label htmlFor={`choice-${idx}`} className="multiple-question-label">
+                <RadioButtonUncheckedIcon className="multiple-question-mark"/>    
+            </label>
+            {/* <RadioButtonUncheckedIcon className="multiple-question-mark"/> */}
+            <input className="multiple-question-input" type="text" id={`choice-${idx}`} defaultValue={`옵션 ${idx}`}/>
+            <CloseRoundedIcon className="choice-delete-icon"/>
+        </div>
+    )
+}
 
 const MultipleQuestion = () => {
     return (
         <div className="multiple-question-wrapper">
-            multiple
+            <MultipleQuestionChoice idx={1}/>
+            <MultipleQuestionChoice idx={2}/>
+            <div className="choice-add-indicator">
+                <RadioButtonUncheckedIcon className="choice-add-icon"/>    
+                <div className="choice-add-div">
+                    <span id="add-option">옵션 추가</span>&nbsp;
+                    <span>또는</span>&nbsp;
+                    <span id="add-others">'기타' 추가</span>
+                </div>
+            </div>
         </div>
     )
 }
