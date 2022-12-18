f0fbb011a1c1fdf848e1a56b22ccba3f9c3bd134
jho378 <jho378@naver.com>
2022-12-18 20:38:32 +0900

    [feature] QuestionItemType과 ExplanationItemType에 생성시간에 따른  id 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,15 +5,19 @@ export type QuestionItemType = {
     isFocused : boolean,
     options : string[] | null,
     answer : string | string[],
+    id : number,
 }
 
 export type ExplanationItemType = {
     type : "explanation"
     title : string,
     explanation : string,
+    isRequired : boolean;
     isFocused : boolean
+    id : number,
 }
 
 export type Questionnaire = {
     questions : (QuestionItemType | ExplanationItemType)[],
+    focusedId : number,
 }
