f3459b504807d35740fd76965d0a8e00e846240a
jho378 <jho378@naver.com>
2022-12-18 20:33:49 +0900

    [fix] MULTIPLE 상수 오탈자 수정

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,5 +2,5 @@ export const SHORT = 'short' as string;
 export const LONG = 'long' as string;
 export const DROPDOWN = 'dropdown' as string;
 export const CHECKBOX = 'checkbox' as string;
-export const MULTIPLE = 'multuple' as string;
+export const MULTIPLE = 'multiple' as string;
 export const EXPLANATION = 'explanation' as string;
