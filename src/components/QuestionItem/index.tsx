a4089a281880736942c888307ff227939adafe29
jho378 <jho378@naver.com>
2022-12-18 20:33:32 +0900

    [feature] 설문지의 헤더 부분과 푸터 부분에 props로 questionData 전달 구현

--- src/components/QuestionItem/index.tsx
+++ src/components/QuestionItem/index.tsx
@@ -2,23 +2,52 @@ import QuestionItemHeader from "../QuestionItemHeader";
 import QuestionItemFooter from "../QuestionItemFooter";
 
 import "./QuestionItem.scss";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
+import React from "react";
+import { ExplanationItemType, QuestionItemType } from "../../types";
+import { Dispatch } from "redux";
+import { RootState } from "../../store/slices";
 
 type QuestionItemPropsType = {
     children : ReactNode,
 }
 
 const QuestionItem = ({ children } : QuestionItemPropsType) => {
+    const [questionData, setQuestionData] = useState<QuestionItemType>({
+        type : "long",
+        question : "",
+        isRequired : false,
+        isFocused : false,
+        options : [],
+        answer : "",
+        id : Date.now(),
+    });
+    useEffect(() => {
+        if ( !React.isValidElement(children) ) return;
+        setQuestionData(children.props.questionData);
+    }, [children]) 
+    useEffect(() => {}, [])
     return (
         <div className="question-item-wrapper">
             <div className="question-item-focused"></div>
             <div className="question-item-content">
-                <QuestionItemHeader />
-                    {children}
-                <QuestionItemFooter />
+                <QuestionItemHeader questionData={ questionData }/>
+                {children}
+                <QuestionItemFooter questionData={ questionData }/>
             </div>
         </div>
     )
 }
 
+const mapDisptachToProps = (dispatch : Dispatch) => {
+    return {
+
+    }
+}
+
+const mapStateToProps = (state : RootState) => {
+    return {
+
+    }
+}
 export default QuestionItem;
