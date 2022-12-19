a93f6d9afbaf169eae5258e10e24c1efb3eeabe8
jho378 <jho378@naver.com>
2022-12-19 16:52:15 +0900

    [feature] 제출 및 설문 답안 작성 내용 초기화 구현

--- src/pages/Preview/index.tsx
+++ src/pages/Preview/index.tsx
@@ -3,20 +3,28 @@ import { connect, useDispatch } from "react-redux";
 import { Dispatch } from "redux";
 import { PREVIEW } from "../../constants";
 import { RootState } from "../../store/slices";
-import { updateViewPage } from "../../store/slices/questionnaireSlice";
+import { resetQuesionnaireAnswers, updateViewPage } from "../../store/slices/questionnaireSlice";
+import { useNavigate } from "react-router-dom";
 import "./Preview.scss";
 import Header from "../../components/Header";
 import QuestionItemList from "../../components/QuestionItemList";
 const Preview = ({ questionnaire } : any) => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     useEffect(() => {
         dispatch(updateViewPage({ page : PREVIEW }));
     }, [dispatch])
 
     return (
         <div id="preview-wrapper">
-            <Header />
-            <QuestionItemList />
+            <form id="preview-form" onSubmit={() => navigate('/response')}>
+                <Header />
+                <QuestionItemList />
+                <div id="preview-controller">
+                    <button id="preview-submit-btn" type="submit">제출</button>
+                    <span id="preview-reset" onClick={() => dispatch(resetQuesionnaireAnswers())}>양식 지우기</span>
+                </div>
+            </form>
         </div>
     )
 }
@@ -25,6 +33,7 @@ const Preview = ({ questionnaire } : any) => {
 const mapDispatchToProps = (dispatch : Dispatch) => {
     return {
         updateViewPage : (page : string) => dispatch(updateViewPage(page)),
+        resetQuestionnaireAnswers : () => dispatch(resetQuesionnaireAnswers),
     }
 }
 
